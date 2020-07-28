3ca88fc31fd2bff4bd61daa99b885e6568522057
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-07-28 19:27:38 -0300

    Implemented Produto comp and cont;

--- src/components/App.component.jsx
+++ src/components/App.component.jsx
@@ -4,9 +4,9 @@ import { Container, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import ArquiteturaContainer from '../containers/Arquitetura.container';
 import MarcaContainer from '../containers/Marca.container';
 import ModeloContainer from '../containers/Modelo.container';
-import Loja from '../models/Loja.model';
 import LojaContainer from '../containers/Loja.container';
 import VariacaoContainer from '../containers/Variacao.container';
+import ProdutoContainer from '../containers/Produto.container';
 
 class App extends React.Component {
 	constructor(props) {
@@ -30,6 +30,7 @@ class App extends React.Component {
 						<Route path="/loja" component={LojaContainer} />
 						<Route path="/marca" component={MarcaContainer} />
 						<Route path="/modelo" component={ModeloContainer} />
+						<Route path="/produto" component={ProdutoContainer} />
 						<Route path="/variacao" component={VariacaoContainer} />
 					</Switch>
 				</Container>
---
+++ src/components/Produto.component.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import PropTypes from 'prop-types';
+import icons from './icons';
+import Produto from '../models/Produto.model';
+import Loja from '../models/Loja.model';
+import Variacao from '../models/Variacao.model';
+
+export default class ProdutoComponent extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {};
+	}
+
+	render() {
+		const {
+			actions, produtos, lojas, variacoes,
+		} = this.props;
+
+		const columns = [
+			{ title: 'Loja', field: 'loja', lookup: lojas },
+			{ title: 'Variacao', field: 'variacao', lookup: variacoes },
+			{ title: 'Link', field: 'link', type: 'string' },
+			{ title: 'Status', field: 'status', lookup: { ok: 'ok' } },
+		];
+
+		return (
+			<MaterialTable
+				icons={icons}
+				columns={columns}
+				data={produtos}
+				editable={{
+					onRowAdd: (newData) => new Promise((resolve, reject) => {
+						setTimeout(() => {
+							actions.onAdd(newData);
+							resolve();
+						}, 1000);
+					}),
+					onRowUpdate: (newData, oldData) => new Promise((resolve, reject) => {
+						setTimeout(() => {
+							actions.onUpdate(newData, oldData);
+							resolve();
+						}, 1000);
+					}),
+					onRowDelete: (oldData) => new Promise((resolve, reject) => {
+						setTimeout(() => {
+							actions.onDelete(oldData);
+							resolve();
+						}, 1000);
+					}),
+				}}
+				title="Produtos"
+			/>
+		);
+	}
+}
+
+ProdutoComponent.protoTypes = {
+	produtos: PropTypes.arrayOf(new Produto().Produto()),
+	actions: PropTypes.objectOf({
+		onAdd: PropTypes.func,
+		onUpdate: PropTypes.func,
+		onDelete: PropTypes.func,
+	}),
+	lojas: PropTypes.objectOf(new Loja().Loja()),
+	variacoes: PropTypes.objectOf(new Variacao().Variacao()),
+};
