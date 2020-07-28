3cecb7bfb0418d2b9aecd184f52824a179bd6d2a
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-07-28 20:07:53 -0300

    Added basename to the route;

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import AppContainer from './containers/App.container';
 initializeFirebase();
 
 ReactDOM.render(
-	<Router>
+	<Router basename={process.env.PUBLIC_URL}>
 		<Switch>
 			<Route path="/" component={AppContainer} />
 		</Switch>
