fba840d3bde0a409257e40ad69b07a648765becc
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-07-28 19:49:58 -0300

    Updates

---
+++ precache-manifest.7b1ee63f65de1277937a7adf27227614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c38d54066122249d8614c4d8986a54",
+    "url": "/obshard_plat/index.html"
+  },
+  {
+    "revision": "f431c71ffa31dfcc59d1",
+    "url": "/obshard_plat/static/js/2.7b91b51d.chunk.js"
+  },
+  {
+    "revision": "b3279ba86c4df88f610a35288d4396f2",
+    "url": "/obshard_plat/static/js/2.7b91b51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96d352d5bfabd6da7f3",
+    "url": "/obshard_plat/static/js/main.28915de2.chunk.js"
+  },
+  {
+    "revision": "39dd93e325fc0f7b1811",
+    "url": "/obshard_plat/static/js/runtime-main.716f9b51.js"
+  }
+]);
