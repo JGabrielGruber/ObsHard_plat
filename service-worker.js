7a33c852288d3079399479697c3f1153543dcc1e
JGabrielGruber <JGabrielGruber@users.noreply.github.com>
2020-07-29 18:53:04 +0000

    deploy: 3967f4232490a756e1a48e4f56bfeeb7001dd615

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ObsHard_plat/precache-manifest.cb5620abc99f034c4b21ae99fe25b851.js"
+  "/ObsHard_plat/precache-manifest.49dfa97049e4b5e239e712a54f9d8a9b.js"
 );
 
 self.addEventListener('message', (event) => {
