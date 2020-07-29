c1c7a22b541232106af514e58663c63b9af2a6b6
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-07-28 21:23:17 -0300

    Refactored code, moved the apiKey;

--- src/configFirebase.js
+++ src/configFirebase.js
@@ -1,18 +1,16 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-	authDomain: "obsgpu-bot.firebaseapp.com",
-	databaseURL: "https://obsgpu-bot.firebaseio.com",
-	projectId: "obsgpu-bot",
-	storageBucket: "obsgpu-bot.appspot.com",
-	messagingSenderId: "91330971757",
-	appId: "1:91330971757:web:9a73f216a8a94fe00f5764",
+	authDomain: 'obsgpu-bot.firebaseapp.com',
+	databaseURL: 'https://obsgpu-bot.firebaseio.com',
+	projectId: 'obsgpu-bot',
+	storageBucket: 'obsgpu-bot.appspot.com',
+	messagingSenderId: '91330971757',
+	appId: '1:91330971757:web:9a73f216a8a94fe00f5764',
+	apiKey: 'AIzaSyD4zaQXmn_mQCySTEo1I6z8XZMVhb98nL0',
 };
 
-firebase.initializeApp({
-	...firebaseConfig,
-	apiKey: process.env.REACT_APP_APIKEY,
-});
+firebase.initializeApp(firebaseConfig);
 
 export default function initializeFirebase() {
 	return firebase.app;
